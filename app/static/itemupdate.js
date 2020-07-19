39d4f34ce592439a9aa9af83aed2b0031097547a
Moody Rahman <mrahman01@stuy.edu>
2020-07-19 18:08:14 -0400

    character update jquery

--- app/static/itemupdate.js
+++ app/static/itemupdate.js
@@ -77,6 +77,53 @@ function deleteitemfr(elem){
 	post(deleteurl, data);
 }
 
+function charupdate(elem){
+	self=$(elem);
+	console.log(elem);
+	charnamenode = $("#charname")
+	charclassnode = $("#charclass")
+	chardescnode = $("#chardesc")
+
+	charnamefield = $('<input id="charname" placeholder="character name here" type="text" style="height:100%; display : inline; line-height: 2em; font-size:2em;" class="form-control md-form" />')
+	// itemnamefield = $('<textarea style="display : inline;" class="form-control md-form" />')
+	charnamefield.val(charnamenode.text())
+
+	charclassfield = $('<input id="charclass" placeholder="character class here" type="text" style="height:100%; display : inline; line-height: 2em; font-size:2em;" class="form-control md-form" />')
+	charclassfield.val(charclassnode.text())
+
+	// itemdescfield = $(('<input type="text" style="height:100px; display : inline;" class="form-control md-form" />'))
+	chardescfield = $('<textarea id="chardesc" placeholder="character description here" style="display : inline; line-height: 2em;" rows="2" class="form-control md-form" />')
+	chardescfield.val(chardescnode.text())
+
+
+
+	charnamenode.replaceWith(charnamefield)
+	charclassnode.replaceWith(charclassfield)
+	chardescnode.replaceWith(chardescfield)
+
+	self.html("CONFIRM");
+	self.removeAttr("onclick");
+	self.attr("onclick", "charupdatefr(self)");
+}
+
+function charupdatefr(elem){
+	console.log("it's been confirmed")
+	charnamenode = $("#charname")
+	charclassnode = $("#charclass")
+	chardescnode = $("#chardesc")
+
+	data={
+		name:charnamenode.val(), 
+		class:charclassnode.val(),
+		description:chardescnode.val()
+	}
+
+	console.log(data)
+
+	console.log(charupdateurl)
+
+	post(charupdateurl, data)
+}
 
 
 function post(path, params, method = 'post') {
