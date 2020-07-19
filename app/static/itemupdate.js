0cb910dae2383d7697ceb6bcdae8c720c7c7b81b
Moody Rahman <mrahman01@stuy.edu>
2020-07-19 08:32:32 -0400

    delete item js

--- app/static/itemupdate.js
+++ app/static/itemupdate.js
@@ -40,11 +40,39 @@ function updateitem(elem) {
 		"itemname":itemname, 
 		"description":description 
 	};
-	console.log(url);
 	// data = JSON.stringify(data)
 	console.log(data);
 	console.log(itemnamenode)
-	post(url, data);
+	post(updateurl, data);
+}
+
+
+function deleteitem(elem){
+	self = $(elem);
+
+	console.log(self)
+	self.html("CONFIRM?")
+	self.removeAttr("onclick");
+	self.attr("onclick", "deleteitemfr(self)");
+}
+
+function deleteitemfr(elem){
+	self = $(elem);
+	itemid = self.attr("id");
+	itemnamenode = $("#" + itemid + "_name")
+	itemdescriptionnode = $("#" + itemid + "_description")
+	itemname = itemnamenode.text()
+	description = itemdescriptionnode.text()
+
+	data = {
+		"itemid": itemid,
+		"charid": charid,
+		"itemname": itemname,
+		"description": description
+	};
+
+	console.log(data)
+	post(deleteurl, data);
 }
 
 
