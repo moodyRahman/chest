652c347c788faba680595a0baec3a6f48cc6294f
Moody Rahman <mrahman01@stuy.edu>
2020-07-20 13:09:52 -0400

    formatting css id's

--- app/static/charupdate.js
+++ app/static/charupdate.js
@@ -5,15 +5,15 @@ function charupdate(elem) {
 	charclassnode = $("#charclass")
 	chardescnode = $("#chardesc")
 
-	charnamefield = $('<input id="charname" placeholder="character name here" type="text" style="height:100%; display : inline; line-height: 2em; font-size:2em; color:blue" class="form-control md-form" />')
+	charnamefield = $('<input id="charname" placeholder="character name here" type="text" style="height:100%; display : inline; line-height: 2em; font-size:2em; color:green" class="form-control md-form" />')
 	// itemnamefield = $('<textarea style="display : inline;" class="form-control md-form" />')
 	charnamefield.val(charnamenode.text())
 
-	charclassfield = $('<input id="charclass" placeholder="character class here" type="text" style="height:100%; display : inline; line-height: 2em; font-size:2em; color:blue" class="form-control md-form" />')
+	charclassfield = $('<input id="charclass" placeholder="character class here" type="text" style="height:100%; display : inline; line-height: 2em; font-size:2em; color:green" class="form-control md-form" />')
 	charclassfield.val(charclassnode.text())
 
 	// itemdescfield = $(('<input type="text" style="height:100px; display : inline;" class="form-control md-form" />'))
-	chardescfield = $('<textarea id="chardesc" placeholder="character description here" style="display : inline; line-height: 2em; color:blue" rows="2" class="form-control md-form" />')
+	chardescfield = $('<textarea id="chardesc" placeholder="character description here"  rows="2" class="form-control md-form" />')
 	chardescfield.val(chardescnode.text())
 
 
@@ -23,7 +23,7 @@ function charupdate(elem) {
 	chardescnode.replaceWith(chardescfield)
 
 	// chardeletebutton = $('<button class="btn btn-danger" style="float: right;" onclick="chardelete(this)" data-toggle="modal" data-target="#chardeletemodal">DELETE THIS CHARACTER</button>')
-	chardeletebutton = $('<button type="button" class="btn btn-danger" style="float: right;" data-toggle="modal" data-target="#exampleModal">DELETE CHARACTER</button >')
+	chardeletebutton = $('<button type="button" class="btn btn-danger" style="float: right;" data-toggle="modal" data-target="#deletecharmodal">DELETE CHARACTER</button >')
 	chardeletebutton.insertAfter(self)
 	// self.insertAfter(chardeletebutton)
 	self.html("CONFIRM EDIT");
