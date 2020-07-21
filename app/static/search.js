dc56bbf81ea98b9aadafb6cd82e3809455f56a5d
Moody Rahman <mrahman01@stuy.edu>
2020-07-21 15:22:08 -0400

    removed prints

--- app/static/search.js
+++ app/static/search.js
@@ -1,13 +1,10 @@
-
 function isvalidsearch(query, name, description, tags){
 	if(name.includes(query)){
 		return true;
 	}
-	
 	if (description.includes(query)){
 		return true;
 	}
-
 	for (let x = 0; x < tags.length; x++) {
 		const tag = tags[x];
 		if (tag.includes(query)) {
@@ -15,7 +12,6 @@ function isvalidsearch(query, name, description, tags){
 			return true
 		}
 	}
-
 	return false;
 }
 
@@ -29,29 +25,17 @@ $("#searchbox").on("input", function(self){
 			return
 		}
 		itemid = this.getAttribute("id")
-		
 		name = $("#" + itemid + "_name").text().toLowerCase()
 		description = $("#" + itemid + "_description").text().toLowerCase()
 		tags = []
 		$("#" + itemid + "_tags").children().each(function(index){
-			// console.log(this.innerHTML)
 			tags.push(this.innerHTML.toLowerCase())
 		})
-		// console.log(this)
-		// console.log(name)
-		// console.log(description)
-		// console.log(tags)
 		if (isvalidsearch(query, name, description, tags)){
 			this.style.display = ""
-
-			console.log(this)
 		}
 		else{
 			this.style.display = "none"
 		}
-		// console.log(isvalidsearch(query, name, description, tags))
-		// console.log("================")
-
 	})
-
 })
